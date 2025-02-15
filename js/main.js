ff36383dfdd524d1d6a6d760fa367415ebba76a8
samiansari01 <samiansari130@gmail.com>
2025-02-15 23:35:38 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+// form handle
+
